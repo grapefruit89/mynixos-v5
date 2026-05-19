168e8d28ba228304a69c8dc2ea81a619df1f0209
grapefruit89 <49215401+grapefruit89@users.noreply.github.com>
2026-05-19 18:20:44 +0200

    fix(kernel): complete filesystem protection (fifos, regular)

--- modules/core/kernel-hardening.nix
+++ modules/core/kernel-hardening.nix
@@ -143,6 +143,8 @@ in
       # Filesystem Hardening
       "fs.protected_hardlinks" = 1;
       "fs.protected_symlinks" = 1;
+      "fs.protected_fifos" = 2;
+      "fs.protected_regular" = 2;
       
       # ASLR & Memory Hardening
       "vm.mmap_rnd_bits" = 32;
