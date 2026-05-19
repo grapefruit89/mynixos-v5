a224ded2646ce040f6667ccae62689b3ac1d5d2f
grapefruit89 <49215401+grapefruit89@users.noreply.github.com>
2026-05-19 13:32:22 +0200

    chore(hardware): remove unused registry.nix

--- hardware/q958/registry.nix
+++
@@ -1,9 +0,0 @@
-{ lib, ... }: {
- options.my.profiles.hardware = {
- q958.enable = lib.mkOption {
- type = lib.types.bool;
- default = true;
- description = "Enable Fujitsu Q958 hardware profile";
- };
- };
-}
